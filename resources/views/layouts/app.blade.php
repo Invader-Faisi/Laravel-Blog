a9fb361d1952fdf518603833ea5f3db0ef9e13c0
Invader-Faisi <code.pedia.tube@gmail.com>
2023-10-16 16:22:22 +0500

    upvote and downvote

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
        <!-- Font Awesome -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
             integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
-        <!-- Scripts -->
+        @livewireStyles
+            <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         </head>
 
@@ -120,7 +121,7 @@
             </div>
         </footer>
 
-
+        @livewireScripts
     </body>
 
 </html>
